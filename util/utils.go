0d76aad722df11129c7ad547f036a3f3160b7859
Zeeshan Abid <zeeshan.abid@wrapp.com>
2017-03-03 18:01:41 +0100

    Add utils

---
+++ util/utils.go
@@ -0,0 +1,13 @@
+package util
+
+type ErrFunc func() error
+
+// A handy method to short circuit if any error happens when executing functions
+func ShortCurcuit(funcs ...ErrFunc) error {
+	for _, f := range funcs {
+		if err := f(); err != nil {
+			return err
+		}
+	}
+	return nil
+}

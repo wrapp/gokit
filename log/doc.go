e0c9941c4b0e9a117439fb10390546e79d58a091
Zeeshan Abid <zeeshan.abid@wrapp.com>
2017-03-17 17:39:47 +0100

    Add docs for log formatter

--- log/doc.go
+++ log/doc.go
@@ -1,2 +1,3 @@
-// structured logging
+// `log` package provides a default log formatter which prints the log in JSON format on
+// stdout. This also adds some extra fields which are added in the log entry before printing.
 package log
